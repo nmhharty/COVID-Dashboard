710f832c35d811f98f3773e642a4de00ec26a60a
Nicole Harty <nharty@routt.gov>
2022-02-02 09:30:39 -0700

    trimming down the scope of data presented on dashboard

--- KnitAndDeployCOVIDdashboard.R
+++ KnitAndDeployCOVIDdashboard.R
@@ -16,30 +16,30 @@ library(openxlsx)
 
 
 ##Set Parameters
-INTERNALparameters <- list(DataThroughDate = "2022-01-21",
-     MostRecentWeekStart = "2022-01-17",
+INTERNALparameters <- list(DataThroughDate = "2022-01-30",
+     MostRecentWeekStart = "2022-01-24",
      RouttPop = 25652,
-     DataUpdateDate = "2022-01-23",
+     DataUpdateDate = "2022-02-01",
      Internal = TRUE)
 
 
-PUBLICparameters <- list(DataThroughDate = "2022-01-21",
-                         MostRecentWeekStart = "2022-01-10",
+PUBLICparameters <- list(DataThroughDate = "2022-01-30",
+                         MostRecentWeekStart = "2022-01-24",
                          RouttPop = 25652,
-                         DataUpdateDate = "2022-01-23",
+                         DataUpdateDate = "2022-02-01",
                            Internal = FALSE)
 
 #use below when testing/troubleshooting indiv RMD files
-# params <- list(DataThroughDate = "2022-01-07",
-#                MostRecentWeekStart = "2022-01-03",
+# params <- list(DataThroughDate = "2022-01-25",
+#                MostRecentWeekStart = "2022-01-17",
 #                RouttPop = 25652,
-#                DataUpdateDate = "2022-01-09",
+#                DataUpdateDate = "2022-01-27",
 #                Internal = FALSE)
 
 # INTERNAL Dashboard --------------------------------------------------
 
 rmarkdown::render(
-  'Dashboard_PARENT.Rmd',
+  'Dashboard_PARENT_PRUNE.Rmd',
   output_file = paste0('INTERNAL Routt County COVID Dashboard.html'),
   params = INTERNALparameters,
   envir = parent.frame()
@@ -50,7 +50,7 @@ file.copy("INTERNAL Routt County COVID Dashboard.html", "INTERNAL Dashboard", ov
 # PUBLIC Dashboard --------------------------------------------------
 # 
 rmarkdown::render(
-  'Dashboard_PARENT.Rmd',
+  'Dashboard_PARENT_PRUNE.Rmd',
   output_file = paste0('PUBLIC Routt County COVID Dashboard.html'),
   params = PUBLICparameters,
   envir = parent.frame()
@@ -80,6 +80,3 @@ deployApp(
   appId = 3283594,
   forceUpdate = TRUE
 )
-
-
-  
